e94571601a9a8dbb7f82d8d3f709f16a2ba5f213
kl2400030727 <snehikurapati@gmail.com>
2025-08-18 13:44:20 +0530

    Snehi

---
+++ Example1.js
@@ -0,0 +1,31 @@
+// var city="London";
+// let country="UK";
+// const pi=3.14;
+// console.log(country)
+// console.log(city)
+// let a1=100
+// let a2=200
+// console.log(a1+a2)
+
+let sum=5+3;
+let isEqual=(sum===8);
+console.log(isEqual)
+
+let assignVar=10;
+assignVar+=5;
+console.log(assignVar)
+
+let str="Hello";
+let num=42;
+let obj={name:"john"};
+let arr=[1,2,3];
+
+let attendance=60+5;
+let eligibleForExam=(attendance>=75);
+console.log("Eligible for exam?",eligibleForExam);
+
+function greetStudent(name)
+{
+    return "Welcome " + name + " KL University!";
+}
+console.log(greetStudent("Snehi"));
