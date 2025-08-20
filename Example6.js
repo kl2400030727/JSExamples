7eee2327fcacf1b171ea014a039444c7de23bd23
kl2400030727 <snehikurapati@gmail.com>
2025-08-20 12:24:15 +0530

    20-08-205

---
+++ Example6.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Enter a number: ",function(answer){
+    const number = parseInt(answer);
+    if(number>0){
+        console.log("The number is positive");
+    }else if(number ===0){
+        console.log("The number is zero");
+    }else{
+        console.log("The number is negative");
+    }
+    rl.close();
+});
